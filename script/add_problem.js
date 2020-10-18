9b69408e4ba941200fd0755672a3d46695096055
CsYakamoz <532609418@qq.com>
2020-10-18 16:39:26 +0800

    feat: script/add_problem.js will crawl problem info

--- script/add_problem.js
+++ script/add_problem.js
@@ -1,13 +1,103 @@
 const { join, resolve } = require('path');
 const { writeFileSync, mkdirSync } = require('fs');
-const enquirer = require('enquirer');
+const { prompt } = require('enquirer');
+
+const crawlProblemInfo = require('./crawl_problem_info');
 
 const PROJECT_ROOT = join(resolve(__dirname, '..'));
 const ALG_DIR = join(PROJECT_ROOT, 'algorithms');
+const LEETCODE_SUPPORTED_LANGUAGE = [
+    { name: 'javascript', isFamiliar: true },
+    { name: 'cpp', isFamiliar: false },
+    { name: 'java', isFamiliar: false },
+    { name: 'python', isFamiliar: false },
+    { name: 'python3', isFamiliar: false },
+    { name: 'c', isFamiliar: false },
+    { name: 'csharp', isFamiliar: false },
+    { name: 'ruby', isFamiliar: false },
+    { name: 'swift', isFamiliar: false },
+    { name: 'golang', isFamiliar: false },
+    { name: 'scala', isFamiliar: false },
+    { name: 'kotlin', isFamiliar: false },
+    { name: 'rust', isFamiliar: false },
+    { name: 'php', isFamiliar: false },
+    { name: 'typescript', isFamiliar: false },
+];
 
-const javascript = (problemDir, problemName) => {
+// call the main function on the last line
+const main = async () => {
+    const { problemLink, language } = await prompt([
+        {
+            type: 'input',
+            name: 'problemLink',
+            message: 'problem link',
+        },
+        {
+            type: 'select',
+            name: 'language',
+            message: 'which language do you use',
+            choices: LEETCODE_SUPPORTED_LANGUAGE.filter(
+                (item) => item.isFamiliar
+            ).map((item) => item.name),
+        },
+    ]);
+
+    const {
+        problemName,
+        description,
+        tags,
+        code,
+        difficulty,
+    } = await crawlProblemInfo(problemLink, language);
+
+    const dirName = problemName
+        .replace(/[.-\s]+/g, '-')
+        .replace(/^(\d+)/, (match) => match.padStart(4, '0'));
+
+    const problemDir = join(ALG_DIR, dirName);
+    mkdirSync(problemDir);
+
+    generateREADME(
+        problemDir,
+        problemName,
+        problemLink,
+        description,
+        difficulty,
+        tags
+    );
+
+    const mapping = { javascript };
+    mapping[language](problemDir, problemName, code);
+};
+
+const generateREADME = (
+    problemDir,
+    problemName,
+    problemLink,
+    description,
+    difficulty,
+    tags
+) => {
+    writeFileSync(
+        join(problemDir, 'README.md'),
+        [
+            `## [${problemName}](${problemLink})`,
+            '',
+            '### Description',
+            '',
+            description,
+            '',
+            `**Difficulty:** \`${difficulty}\``,
+            '',
+            `**Tags:** ${tags.map((item) => `\`${item}\``).join(' ')}`,
+            '',
+        ].join('\n')
+    );
+};
+
+const javascript = (problemDir, problemName, code) => {
     mkdirSync(join(problemDir, 'javascript'));
-    writeFileSync(join(problemDir, 'javascript', 'index.js'), '');
+    writeFileSync(join(problemDir, 'javascript', 'index.js'), code);
 
     mkdirSync(join(problemDir, 'javascript', 'test'));
     writeFileSync(
@@ -22,38 +112,4 @@ const javascript = (problemDir, problemName) => {
     );
 };
 
-(async () => {
-    const { problemName, problemLink, language } = await enquirer.prompt([
-        {
-            type: 'input',
-            name: 'problemName',
-            message: 'problem name',
-        },
-        {
-            type: 'input',
-            name: 'problemLink',
-            message: 'problem link',
-        },
-        {
-            type: 'select',
-            name: 'language',
-            message: 'which language do you use',
-            choices: ['javascript'],
-        },
-    ]);
-
-    const dirName = problemName
-        .replace(/[.-\s]+/g, '-')
-        .replace(/^(\d+)/, (match) => match.padStart(4, '0'));
-
-    const dirPath = join(ALG_DIR, dirName);
-    mkdirSync(dirPath);
-    writeFileSync(
-        join(dirPath, 'README.md'),
-        `## [${problemName}](${problemLink})\n\n### Description\n\n`
-    );
-
-    const mapping = { javascript };
-
-    mapping[language](dirPath, problemName);
-})();
+main().catch((e) => console.log(e.stack));
---
+++ script/crawl_problem_info.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const titleSlugRegex = /https:\/\/leetcode\.com\/problems\/(?<titleSlug>.*)\//;
+
+/**
+ * @param {string} problemLink
+ */
+module.exports = async (problemLink, language) => {
+    const titleSlug = problemLink.match(titleSlugRegex).groups.titleSlug;
+    const body = {
+        operationName: 'questionData',
+        variables: { titleSlug },
+        query:
+      'query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    __typename\n  }\n}\n',
+    };
+
+    const {
+        data: { question },
+    } = await fetch('https://leetcode.com/graphql', {
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+        method: 'POST',
+    }).then((res) => res.json());
+
+    return {
+        problemName: question.questionFrontendId + '. ' + question.title,
+        description: question.content.replace(/\r\n/g, '\n'),
+        tags: question.topicTags.map((item) => item.name),
+        code: question.codeSnippets.find((item) => item.langSlug === language).code,
+        difficulty: question.difficulty,
+    };
+};
